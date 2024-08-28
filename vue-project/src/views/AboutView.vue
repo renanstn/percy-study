a87f8a458945e20451e6a56dcc443fbf49cf4514
Renan <renan_ota@hotmail.com>
2024-08-27 21:35:06 -0300

    Add about page

---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,52 @@
+<script setup></script>
+
+<template>
+  <main>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus maximus
+      cursus orci, a accumsan lectus bibendum auctor. Maecenas sollicitudin
+      ligula ac enim ultrices maximus. Sed pharetra, sapien at pellentesque
+      bibendum, turpis massa fermentum elit, sed pharetra erat diam eu sapien.
+      Vivamus at augue eu nunc sollicitudin mattis. Nam finibus imperdiet sem,
+      ut luctus ex pellentesque at. Suspendisse dapibus nisi quis viverra
+      lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+      posuere cubilia curae; Aliquam erat volutpat. In tincidunt metus mattis
+      augue porttitor dictum. Aliquam erat volutpat. Integer mattis odio ut nisl
+      efficitur volutpat. Sed non libero eget urna consectetur mattis bibendum
+      at neque. Aliquam erat volutpat. Mauris faucibus felis vitae aliquet
+      dictum. Proin vel nunc felis. Vestibulum nulla augue, porta id metus a,
+      ornare finibus dolor. Fusce ac tortor mattis, consequat lorem eu, vehicula
+      urna. Curabitur facilisis blandit blandit. Curabitur euismod porttitor
+      ante, sed rutrum nibh bibendum in. Integer et interdum ante. Phasellus
+      faucibus faucibus risus, et suscipit ante placerat vel. Quisque gravida
+      dignissim lorem id iaculis. In tempor at purus sit amet ultrices.
+      Pellentesque sit amet nunc accumsan, consequat ex sit amet, venenatis
+      massa. Vestibulum sit amet purus risus. Etiam nulla leo, consectetur et
+      finibus in, porttitor eget ligula. Praesent vitae varius nulla, eu
+      bibendum ligula. Sed nec turpis sodales, bibendum massa eu, ornare dui.
+      Nam pretium eget lorem vitae mollis. Donec sit amet sollicitudin sem.
+      Etiam vel enim iaculis, porttitor risus eget, pharetra lacus. Aenean in
+      tortor tellus. Mauris laoreet mi id sapien porttitor, vel ultrices sem
+      ullamcorper. Duis porttitor tellus ac metus dictum, non bibendum mauris
+      sagittis. Suspendisse non tristique urna, ut varius lectus. Vivamus
+      sodales enim lacus, eu iaculis dui auctor at. Proin suscipit dolor orci,
+      quis semper leo malesuada id. Cras ullamcorper orci nulla. Etiam nec lacus
+      nec purus ultrices mollis. Duis cursus, nulla nec elementum laoreet, dui
+      quam posuere dolor, eu accumsan enim felis et libero. Cras in neque ut ex
+      dictum ornare quis eu nulla. In sodales fermentum nulla, ac porta lectus
+      sagittis quis. Phasellus vulputate rutrum felis. In purus nisi, feugiat
+      eget mauris in, ultricies pharetra enim. Vestibulum nec porta justo. Duis
+      vel tempus sapien, id aliquet augue. Suspendisse eu lectus sit amet ligula
+      maximus semper. Nulla massa lectus, tempor vel molestie ut, sodales vitae
+      nulla. Donec ut lorem vitae sapien vulputate porta vitae ac ligula.
+      Pellentesque at varius sem. Aenean urna metus, rutrum ut neque id,
+      vestibulum mattis orci. Etiam viverra neque a enim hendrerit vulputate.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+      eleifend, nulla ut cursus venenatis, velit mauris dictum arcu, sit amet
+      laoreet ligula lectus nec mauris. Duis suscipit, orci quis ornare semper,
+      massa ante consequat turpis, eu porta orci augue in libero. Phasellus eget
+      diam quis odio dictum maximus id vitae est. Curabitur nunc velit, dapibus
+      vitae condimentum mattis, dictum vitae eros.
+    </p>
+  </main>
+</template>
