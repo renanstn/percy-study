61a12983313b12185996a8e331249020a3b12c70
Renan Santana <renan_ota@hotmail.com>
2024-09-12 10:01:55 -0300

    Remove comment

--- vue-project/cypress/e2e/example.cy.js
+++ vue-project/cypress/e2e/example.cy.js
@@ -9,7 +9,6 @@ describe("My First Test", () => {
 
 describe('Integration test with visual testing', function() {
   it('Loads the homepage', function() {
-    // Load the page or perform any other interactions with the app.
     cy.visit("/");
     // Take a snapshot for visual diffing
     cy.percySnapshot('snap A');
